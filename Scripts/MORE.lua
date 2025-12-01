4a14131086d68b45e3873972422dfe06ab720d50
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-12-01 11:25:43 +0800

    1

---
+++ Scripts/MORE.lua
@@ -0,0 +1,19 @@
+﻿local UI = GameMain:GetMod("UIUPUPUP");
+local time = 0;
+local flag = 0;
+
+function UI:OnStep(dt)
+if flag == 0 then
+time = time + dt;
+if time >= 10 then
+flag = 1;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("NpcLingMaxValue").MaxValue = 999999999;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("MaxAge").MaxValue = 99999;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("ComfyTMin").MaxValue = 9999;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("ComfyTMax").MaxValue = 9999;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("ToleranceTMin").MaxValue = 9999;
+CS.XiaWorld.PropertyMgr.Instance:GetDef("ToleranceTMax").MaxValue = 9999;
+end
+end
+end
+
